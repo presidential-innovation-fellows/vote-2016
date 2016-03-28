ef0bfc757f00f416f340151feedd0e7beeb9d77c
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-03-28 15:35:26 -0400

    Add new methods for event tracking

--- assets/js/gaEvents.js
+++ assets/js/gaEvents.js
@@ -1,3 +1,33 @@
+/* global $, ga */
+
+/*
+ * Send Google Analytics vote event
+ * This event is fired with a type and context. It checks for the existance of
+ * the `ga()` method on the window in order to prevent errors with ad-blockers.
+ * @param type String The type of vote event to fire.
+ * @param context String The context of the type of vote even fired.
+ * @return Boolean Can be used to see if the event was fired properly.
+ */
+function gaVoteEvent (type, context) {
+	if ('function' !== typeof window.ga) {
+		return false;
+	}
+	ga('send', 'event', 'vote', type, context);
+	return true;
+}
+
+/*
+ * Send Optimizely track event
+ * This event is fired with a type for the `trackEvent` method for Optimizely. It
+ * handles the checking for the `optimizely` property on the window so only a type
+ * needs to be supplied.
+ * @param type String The type of event to pass to `optimizely#trackEvent`.
+ */
+function sendTrackEvent (type) {
+	window['optimizely'] = window['optimizely'] || [];
+	window.optimizely.push(['trackEvent', type]);
+}
+
 // GA hooks for tracking voting registration conversions
 //
 $('#form-voting').on('click', function() {
